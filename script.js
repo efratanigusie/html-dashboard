d997980cb2b98fc7c399252b3fe92a9bf8688b9d
Etsubdink Awoke <116604020+etsubeta132@users.noreply.github.com>
2024-04-09 18:15:26 +0300

    code modify

---
+++ script.js
@@ -0,0 +1,15 @@
+
+  document.getElementById("customer-approval-card").addEventListener("click", function() {
+    window.location.href = "/inactive account.html";
+  });
+  document.getElementById("Account-Detail-card").addEventListener("click", function() {
+    window.location.href = "/active account.html";
+  });
+  document.getElementById("customer-Account-List").addEventListener("click", function() {
+    window.location.href = "/viewcustomer.html";
+  });
+  document.getElementById("Transaction").addEventListener("click", function() {
+    window.location.href = "/Transacton.html";
+  });
+
+  
